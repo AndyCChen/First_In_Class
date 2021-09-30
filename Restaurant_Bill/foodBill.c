899ab0bcf1ad35b221c1e978796865fc3bc4356c
Andy Chen <acchen@cpp.edu>
2021-09-29 20:11:31 -0700

    added foodBill source code

--- Restaurant_Bill/foodBill.c
+++ Restaurant_Bill/foodBill.c
@@ -9,10 +9,42 @@
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-   double tax, tip; 
+   time_t t;
+   srand((unsigned) time(&t));
+
+   double taxPercent, tipPercent, taxAmount, tipAmount, total;
+
+   int mealNumber = rand() % 4;
+
+   const char *meal[] = {"Salad", "Soup", "Sandwich", "Pizza"};
+   double mealPrice[] = {9.95, 4.55, 13.25, 22.35};
+
+   double mealCost = mealPrice[mealNumber];
+
+   printf(meal[mealNumber]);
+   printf(" : $%.2lf\n", mealPrice[mealNumber]);
+   printf("-----------------------------\n");
+
+   printf("Please enter tax percent: ");
+   scanf("%lf", &taxPercent);
+
+   printf("Please enter tip percent: ");
+   scanf("%lf", &tipPercent);
+
+   printf("-----------------------------\n\n");
+
+   taxAmount =  mealCost * (taxPercent / 100);
+   tipAmount =  mealCost * (tipPercent / 100);
+   total = mealCost + taxAmount + tipAmount;
+
+   printf("Tax amount: $%.2lf\n", taxAmount);
+   printf("Tip amount $%.2lf\n", tipAmount);
+   printf("Total bill: $%.2lf\n", total);
 
    return 0;
 }
