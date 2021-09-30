8dbaf40cfcb1a38b5fc3be2af48bd2b8834293c6
Andy Chen <acchen@cpp.edu>
2021-09-29 18:12:49 -0700

    created new outline

---
+++ outline.c
@@ -0,0 +1,21 @@
+// Program 1: Restautrant Bill
+
+/*
+   - ask user inputs for tax and tip as percent
+   - random number generator to choose which four meals to use
+   - Total = mealPrice + tax + tip
+   - output the meal cost, tax, tip, and total bill
+
+**/
+
+
+  // Program 2: Number Guessing Game
+
+/* 
+   - create switch statement for menu selection
+   - enclose switch statement in do while loop so user can revisit the menu
+   - option one allows user to play with default 1-10 number generation
+   - option two allows user to specify a upper bound for the number generation 
+     that is not greater than the max value
+   - option three exits the game
+**/   
